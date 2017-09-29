a6aa68c0c5d81a1d7e65e6b7fbb752ac5c06d561
georgeperez011 <32206258+georgeperez011@users.noreply.github.com>
2017-09-29 15:41:07 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        double x = 50;
+        double y = 32;
+        System.out.print("50 divided by 32 is: ");
+        System.out.print(x / y);
+
+
+    }
+}
